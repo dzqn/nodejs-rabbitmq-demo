2becdb59491ded414960b6018157b6498f9f0b0f
Düzgün Tutar <duzguntutar@gmail.com>
2020-09-05 21:26:55 +0300

    ...

--- consumer.js
+++ consumer.js
@@ -1,1 +1,28 @@
-import amqp from "amqp";
+import { connect } from "amqplib";
+const data = require("./data.json")
+
+const queueName = process.argv[2] || "duzgunq";
+
+connect_rabbitmq();
+
+async function connect_rabbitmq() {
+    try {
+        const connection = await connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const assertion = await channel.assertQueue(queueName);
+
+        // Mesajın Alınması...
+        console.log("Mesaj bekleniyor...");
+        channel.consume(queueName, message => {
+            const messageInfo = JSON.parse(message.content.toString());
+
+            const userInfo = data.find(u => u.id == messageInfo.description);
+            if (userInfo) {
+                console.log("İşlenen Kayıt", userInfo);
+                channel.ack(message);
+            }
+        });
+    } catch (error) {
+        console.log("Error", error);
+    }
+}
--- publisher.js
+++ publisher.js
@@ -1,1 +1,26 @@
-import amqp from "amqp";
+import { connect } from "amqplib";
+import { forEach } from "./data.json";
+
+const queueName = process.argv[2] || "duzgunq";
+const message = {
+    description: ""
+};
+
+connect_rabbitmq();
+
+async function connect_rabbitmq() {
+    try {
+        const connection = await connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const assertion = await channel.assertQueue(queueName);
+
+        forEach(i => {
+            message.description = i.id;
+            channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
+            console.log("Gonderilen Mesaj", i.id);
+        })
+
+    } catch (error) {
+        console.log("Error", error);
+    }
+}
