ecf20766e8acaf4d247e13de970f93a160293d29
Düzgün Tutar <duzguntutar@gmail.com>
2020-09-05 22:54:24 +0300

    consumer and publisher

--- consumer.js
+++ consumer.js
@@ -1,22 +1,22 @@
-import { connect } from "amqplib";
-const data = require("./data.json")
+const amqp = require("amqplib");
 
 const queueName = process.argv[2] || "duzgunq";
+const data = require("./data.json");
 
 connect_rabbitmq();
 
 async function connect_rabbitmq() {
     try {
-        const connection = await connect("amqp://localhost:5672");
+        const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createChannel();
         const assertion = await channel.assertQueue(queueName);
 
         // Mesajın Alınması...
         console.log("Mesaj bekleniyor...");
         channel.consume(queueName, message => {
-            const messageInfo = JSON.parse(message.content.toString());
-
-            const userInfo = data.find(u => u.id == messageInfo.description);
+            const messageInfo = JSON.parse(message.content.toString())
+            //   {description : 12}
+            const userInfo = data.find(u => u.id == messageInfo.description)
             if (userInfo) {
                 console.log("İşlenen Kayıt", userInfo);
                 channel.ack(message);
--- publisher.js
+++ publisher.js
@@ -1,24 +1,24 @@
-import { connect } from "amqplib";
-import { forEach } from "./data.json";
+const amqp = require("amqplib");
+const data = require("./data.json");
 
-const queueName = process.argv[2] || "duzgunq";
 const message = {
     description: ""
 };
+const queueName = process.argv[2] || "duzgunq";
 
 connect_rabbitmq();
 
 async function connect_rabbitmq() {
     try {
-        const connection = await connect("amqp://localhost:5672");
+        const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createChannel();
         const assertion = await channel.assertQueue(queueName);
 
-        forEach(i => {
+        data.forEach(i => {
             message.description = i.id;
             channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
             console.log("Gonderilen Mesaj", i.id);
-        })
+        });
 
     } catch (error) {
         console.log("Error", error);
